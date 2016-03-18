3abb63c3cf498f43bd46b5a580e360bd339d8956
Brian Ballsun-Stanton <brian@fedarch.org>
2016-03-18 17:56:02 +1100

    column rename

--- shapefile.py
+++ shapefile.py
@@ -714,6 +714,8 @@ def outputTable(tablename, query):
             csv_writer = UnicodeWriter(open(exportDir+"%s.csv" % (tablename), "wb+"))
             csv_writer.writerow([i[0] for i in tableCursor.description]) # write headers
             csv_writer.writerows(tableCursor)
+    except OperationalError as detail:
+        print tablename, detail
     except:
         print "Unexpected error:", sys.exc_info()[0]
         print tablename, query
